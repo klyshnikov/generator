90723315ab53c003fe4eb63a1fe1d4d3ba2d2d29
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-03-04 15:30:56 +0300

    del

--- product-storage-client-starter/src/main/java/ru/hse/product/storage/client/config/ProductClientConfig.java
+++
@@ -1,8 +0,0 @@
-package ru.hse.product.storage.client.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "ru.hse.product.storage.client")
-public class ProductClientConfig {}
